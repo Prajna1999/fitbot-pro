8d4199161ae7e24a55230eea0197b445fac128c5
Prajna1999 <gituprajna20@gmail.com>
2023-08-21 11:50:25 +0530

    add: streamlit

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text=""
+
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+        
+
+
 
 
 def main():
-    print("Starting")
+    load_dotenv()
+    st.set_page_config(page_title="Your personal fitness trainer")
+    st.header("chat with your fitness trainer")
+    st.text_input("Ask a question about your fitness trainer")
 
-if __name__ == "__main__":
+    with st.sidebar:
+        st.subheader("Your fitness trainer")
+        pdf_docs=st.file_uploader("Upload your pdf here and click on 'Process'", accept_multiple_files=True)
+
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+
+                # get pdf text
+                raw_text=get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+
+                    
+
+                #get the text chunks
+
+                #create vector store
+
+
+
+if __name__=="__main__":
     main()
